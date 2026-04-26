3c0f7d021c571cf40826bb12feb8db1c008d9cd8
nakanishi <p-p2@outlook.jp>
2026-04-26 11:47:42 +0900

    chore: 不要ファイル除外 + バグ修正コード追加

---
+++ PriceCalculator.cs
@@ -0,0 +1,14 @@
+﻿public class PriceCalculator
+{
+    public int CalculateTotal(int price, int quantity)
+    {
+        int total = 0;
+
+        for (int i = 0; i < quantity; i++)
+        {
+            total += price;
+        }
+
+        return total;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var calculator = new PriceCalculator();
+
+        int price = 100;
+        int quantity = 3;
+
+        int total = calculator.CalculateTotal(price, quantity);
+
+        Console.WriteLine($"合計金額: {total}");
+        Console.ReadLine();
+    }
+}
